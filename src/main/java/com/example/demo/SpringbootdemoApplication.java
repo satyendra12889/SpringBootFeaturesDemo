717b35ece5624ee8b428ed090a794469f48f6922
satyendra128 <satyendra@nagarro.com>
2020-05-20 15:02:12 +0530

    loging implemented

--- src/main/java/com/example/demo/SpringbootdemoApplication.java
+++ src/main/java/com/example/demo/SpringbootdemoApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootdemoApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(SpringbootdemoApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
+		
+		logger.debug("This is debug message");
+		logger.info("This is info message");
+		logger.warn("This is warn message");
+		logger.error("This is error message");
+		
 	}
 
 }
